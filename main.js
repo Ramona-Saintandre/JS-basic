f758ea7f684c86de6ddc5b6645602ec899987d2a
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 11:29:15 -0500

    worked on variable declaration

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+        // a variable is just something that lets us store a value 
+        // let myfavoritenumber = 274;
+        // console.log(myfavoritenumber);
+        // let myfavoritenumber = 274;
+        // console.log(myfavoritenumber);
+
+        // a variable can be declared by using keywords
+
+        let somefunnything
+        console.log(somefunnything);
+        somefunnything = -54;
+        console.log(somefunnything);
+
         
