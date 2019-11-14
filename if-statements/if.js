ddc2a37cde5216e40b005c7b472ba910b207ba09
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 17:18:18 -0500

    set up if folder for if exercises

---
+++ if-statements/if.js
@@ -0,0 +1,9 @@
+//an if condition is somethin that will allow us to execute one piece of conde
+//based on the result of some test  (boolean)
+
+if (true) {
+    console.log('It was true');
+}
+if (false) {
+    console.log('The thing inside is true');
+}
--- numbers/basic.js
+++ numbers/basic.js
@@ -1,9 +1,11 @@
 //basic number operation
 
-console.log(2+3);
-console.log(7-3);
-console.log(12*12);
-console.log(9/3);
-console.log(6%9);
-console.log(2+3%75);
+// console.log(2+3);
+// console.log(7-3);
+// console.log(12*12);
+// console.log(9/3);
+// console.log(6%9);
+// console.log(2+3%75);
+
+
 
