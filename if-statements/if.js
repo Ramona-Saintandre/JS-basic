53529e33e7660d582e8fc62c66e745f689223a6f
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 17:30:07 -0500

    stopped at 90.05, if 1 liner

--- if-statements/if.js
+++ if-statements/if.js
@@ -1,9 +1,19 @@
 //an if condition is somethin that will allow us to execute one piece of conde
 //based on the result of some test  (boolean)
 
-if (true) {
-    console.log('It was true');
-}
-if (false) {
-    console.log('The thing inside is true');
-}
+// if (true) {
+//     console.log('It was true');
+// }
+// if (false) {
+//     console.log('The thing inside is true');
+// }
+
+//write a program that prints out "odd!!!" if a number is onloadeddata, and "even!!! " if even
+
+let equateNum = 26;
+if (equateNum % 2 === 0){
+    console.log('Even!!!')
+};
+if (equateNum % 2 === 1){
+    console.log('Odd!!!')
+};
