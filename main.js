44e572677b500fe23cb12f72c2d4133c342fe859
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 14:32:33 -0500

    tried to print 3 numbers with 1 variable

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
         // a variable is just something that lets us store a value 
         // let myfavoritenumber = 274;
         // console.log(myfavoritenumber);
-        // let myfavoritenumber = 274;
+        // myfavoritenumber = 22;
         // console.log(myfavoritenumber);
 
         // a variable can be declared by using keywords
 
-        let somefunnything
-        console.log(somefunnything);
-        somefunnything = -54;
-        console.log(somefunnything);
+        // let somefunnything
+        // console.log(somefunnything);
+        // somefunnything = -54;
+        // let somefunthing = 'hheeeeeheeehehehe fun happens here'
+        // console.log(somefunthing);
 
+        // constants have only 1 value EVER
+
+        // const const4Mona = 'some value to start with'
+        // console.log(onst4Mona);
+        // console.log(onst4Mona);
+
+        /** 
+         * code readability 
+        1. comment confusing code 
+        */
+
+        // write a program that stores any 3 # and print
+
+        // let num1 = 8;
+        // let num2 = 15;
+        // let num3 = 49;
+        // console.log(num1, num2, num3);
         
+
+        /**
+         * write one variable that prints 3 different numbers
+         */
+
+let numString = 49;
+numString = 15;
+numString = 45;
+// print out the first number
+
+console.log('first number',numString);
+console.log('second number',numString);
+console.log('third number',numString);
