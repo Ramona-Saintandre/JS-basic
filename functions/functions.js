e336a8441abc23467257a4cb21b795293638b1dd
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-18 08:25:14 -0500

    function expression not working
    not sure why divideByN is not returning

---
+++ functions/functions.js
@@ -0,0 +1,13 @@
+//function declaration 
+// function calculateTip (price, percentage){
+
+// console.log('calculating tip');
+// return (percentage/100) * price;
+// }
+// calculateTip(18,50);
+
+//function expression
+let divideByN = function(number, n){
+return number / n;
+}
+divideByN(18,3);
--- loops/functions/functions.js
+++
@@ -1,7 +0,0 @@
-//function declaration 
-function calculateTip (price, percentage){
-
-console.log('calculating tip');
-return (percentage/100) * price;
-}
-calculateTip(18,50);
